f231979d6ba0a376e48f2db0445d3a0e435dbbe9
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-04-19 15:20:48 +0800

    GREEN: applySpec > retains the highest arity

--- src/apply-spec.js
+++ src/apply-spec.js
@@ -7,8 +7,23 @@ function checkPlainObject(value) {
 }
 
 function applySpec(specification) {
-    return function (...args) {
+    let highestArity = 0;
+
+    if (checkPlainObject(specification)) {
+        highestArity = Object.entries(specification).reduce(
+            (accumulator, [key, value]) => {
+                return Math.max(accumulator, value.length);
+            },
+            0
+        );
+    }
+
+    function partiallyAppliedFunction(...args) {
+        let results = {};
+
         if (Array.isArray(specification)) {
+            results = [];
+
             return specification.map((value) => {
                 if (typeof value === 'function') {
                     return value(...args);
@@ -16,24 +31,28 @@ function applySpec(specification) {
                     return applySpec(value)(...args);
                 }
             });
-        }
-
-        const results = {};
-
-        for (const [key, value] of Object.entries(specification)) {
-            if (typeof value === 'function') {
-                results[key] = value(...args);
-            } else if (checkPlainObject(value)) {
-                results[key] = applySpec(value)(...args);
-            } else if (Array.isArray(value)) {
-                results[key] = value.map((currentSpec) => {
-                    return applySpec(currentSpec)(...args);
-                });
+        } else if (checkPlainObject(specification)) {
+            for (const [key, value] of Object.entries(specification)) {
+                if (typeof value === 'function') {
+                    results[key] = value(...args);
+                } else if (checkPlainObject(value)) {
+                    results[key] = applySpec(value)(...args);
+                } else if (Array.isArray(value)) {
+                    results[key] = value.map((currentSpec) => {
+                        return applySpec(currentSpec)(...args);
+                    });
+                }
             }
         }
 
         return results;
-    };
+    }
+
+    Object.defineProperty(partiallyAppliedFunction, 'length', {
+        value: highestArity
+    });
+
+    return partiallyAppliedFunction;
 }
 
 export default applySpec;
