5f284e12b5e774d1b02050109bc3bf9cc59b36d2
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-04-19 14:10:16 +0800

    GREEN: works with unary functions

--- src/apply-test.js
+++ src/apply-test.js
@@ -1,6 +1,12 @@
 function applySpec(functor) {
-    return function () {
-        return {};
+    return function (...args) {
+        const newObject = {};
+
+        for (const [key, value] of Object.entries(functor)) {
+            newObject[key] = value(...args);
+        }
+
+        return newObject;
     };
 }
 
