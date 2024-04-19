f3cb33220c1859fc17193253b9dbed36d01782b7
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-04-19 14:00:32 +0800

    RED: applySpec > works with empty spec

---
+++ src/apply-spec.test.js
@@ -0,0 +1,24 @@
+import applySpec from './apply-test';
+import R from 'ramda';
+
+describe('applySpec,', () => {
+    function eq(expected, result) {
+        expect(expected).toEqual(result);
+    }
+
+    it('works with empty spec', function () {
+        eq(applySpec({})(), {});
+    });
+    it.skip('works with unary functions', function () {
+        eq(applySpec({ v: R.inc, u: R.dec })(1), { v: 2, u: 0 });
+    });
+    it.skip('works with binary functions', function () {
+        eq(applySpec({ sum: R.add })(1, 2), { sum: 3 });
+    });
+    it.skip('works with nested specs', function () {
+        eq(applySpec({ unnested: R.always(0), nested: { sum: R.add } })(1, 2), {
+            unnested: 0,
+            nested: { sum: 3 }
+        });
+    });
+});
---
+++ src/apply-test.js
@@ -0,0 +1,3 @@
+function applySpec() {}
+
+export default applySpec;
